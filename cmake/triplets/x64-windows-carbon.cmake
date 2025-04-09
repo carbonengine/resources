a5238bb9715aee5c1995b523b3efb75fec65ef18
hrafn <hrafng@ccpgames.com>
2025-04-09 13:05:07 +0000

    Ensure that we compile with support for the minimum OS versions that we support

--- cmake/triplets/x64-windows-carbon.cmake
+++ cmake/triplets/x64-windows-carbon.cmake
@@ -7,6 +7,10 @@ set(VCPKG_BUILD_TYPE release)
 set(VCPKG_PLATFORM_TOOLSET v143)
 set(VCPKG_CMAKE_SYSTEM_VERSION "10.0.17763.0")
 
+# Verify at compile time that the port supports Windows 10.
+set(VCPKG_C_FLAGS "-DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00")
+set(VCPKG_CXX_FLAGS ${VCPKG_C_FLAGS})
+
 if (PORT MATCHES "bsdiff-drake127")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
