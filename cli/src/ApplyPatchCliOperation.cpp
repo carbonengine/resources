5b77752d8da34c30df308a9fdd8587bd5c71e332
hrafng <5185245+hrafn@users.noreply.github.com>
2025-05-21 16:35:26 +0000

    Fix multipath parameter handling in apply-patch CLI command

--- cli/src/ApplyPatchCliOperation.cpp
+++ cli/src/ApplyPatchCliOperation.cpp
@@ -8,9 +8,9 @@
 ApplyPatchCliOperation::ApplyPatchCliOperation() :
 	CliOperation("apply-patch", "Apply a patch"),
 	m_patchResourceGroupPathArgumentId("--patch-resource-group-path"),
-	m_patchBinariesBasePathArgumentId("--patch-binaries-base-path"),
+	m_patchBinariesBasePathsArgumentId("--patch-binaries-base-path"),
 	m_patchBinariesSourceTypeArgumentId("--patch-binaries-source-type"),
-	m_resourcesToPatchBasePathArgumentId("--resources-to-patch-base-path"),
+	m_resourcesToPatchBasePathsArgumentId("--resources-to-patch-base-path"),
 	m_resourcesToPatchSourceTypeArgumentId("--resources-to-patch-source-type"),
 	m_nextResourcesBasePathArgumentId("--next-resources-base-path"),
 	m_nextResourcesSourceTypeArgumentId("--next-resources-source-type"),
@@ -18,9 +18,9 @@ ApplyPatchCliOperation::ApplyPatchCliOperation() :
 	m_outputDestinationTypeArgumentId("--output-destination-type")
 {
 	AddArgument( m_patchResourceGroupPathArgumentId, "The path to the PatchResourceGroup.yaml file.", true);
-	AddArgument( m_patchBinariesBasePathArgumentId,"The path to the folder containing the patch binaries.", true);
+	AddArgument( m_patchBinariesBasePathsArgumentId,"The paths to the folders containing the patch binaries.", true, true);
 	AddArgument( m_patchBinariesSourceTypeArgumentId,"The type of repository the patch binaries are sourced from.", false, false, "LOCAL_RELATIVE");
-	AddArgument( m_resourcesToPatchBasePathArgumentId,"The path to the folder containing resources to patch.", true);
+	AddArgument( m_resourcesToPatchBasePathsArgumentId,"The paths to the folders containing resources to patch.", true, true);
 	AddArgument( m_resourcesToPatchSourceTypeArgumentId,"The type of repository the resources to patch are sourced from.", false, false, "LOCAL_RELATIVE");
 	AddArgument( m_nextResourcesBasePathArgumentId,"The path to the folder containing the resources after the patch. This is used to download added files, which are not included in the generated patch files.", true);
 	AddArgument( m_nextResourcesSourceTypeArgumentId,"The type of repository the resources after the patch are sourced from.", false, false, "LOCAL_RELATIVE");
@@ -55,12 +55,15 @@ bool ApplyPatchCliOperation::Execute() const
 	}
 
 	std::vector<std::filesystem::path> patchBinarySourceSettingsBasePaths;
-	std::optional<std::string> patchBinaries = m_argumentParser->present<std::string>( m_patchBinariesBasePathArgumentId );
+	auto patchBinaries = m_argumentParser->present<std::vector<std::string>>( m_patchBinariesBasePathsArgumentId );
 	if( !patchBinaries.has_value() )
 	{
 		return false;
 	}
-	patchBinarySourceSettingsBasePaths.push_back( patchBinaries.value() );
+	for( auto path : patchBinaries.value() )
+	{
+		patchBinarySourceSettingsBasePaths.push_back( path );
+	}
 	patchApplyParams.patchBinarySourceSettings.basePaths = patchBinarySourceSettingsBasePaths;
 	patchApplyParams.newBuildResourcesSourceSettings.basePaths = newBuildResourceSettingBasePaths;
 	std::string patchBinariesType = m_argumentParser->get( m_patchBinariesSourceTypeArgumentId );
@@ -70,12 +73,15 @@ bool ApplyPatchCliOperation::Execute() const
 	}
 
 	std::vector<std::filesystem::path> resourcesToPatchSourceSettingsBasePaths;
-	std::optional<std::string> resourcesToPatch = m_argumentParser->present<std::string>( m_resourcesToPatchBasePathArgumentId );
+	auto resourcesToPatch = m_argumentParser->present<std::vector<std::string>>( m_resourcesToPatchBasePathsArgumentId );
 	if( !resourcesToPatch.has_value() )
 	{
 		return false;
 	}
-	resourcesToPatchSourceSettingsBasePaths.push_back( resourcesToPatch.value() );
+	for( auto path : resourcesToPatch.value() )
+	{
+		resourcesToPatchSourceSettingsBasePaths.push_back( path );
+	}
     patchApplyParams.resourcesToPatchSourceSettings.basePaths = resourcesToPatchSourceSettingsBasePaths;
 	std::string resourcesToPatchType = m_argumentParser->get( m_resourcesToPatchSourceTypeArgumentId );
 	if( !StringToResourceSourceType( resourcesToPatchType, patchApplyParams.resourcesToPatchSourceSettings.sourceType ) )
@@ -129,9 +135,9 @@ void ApplyPatchCliOperation::PrintStartBanner(const CarbonResources::ResourceGro
     PrintCommonOperationHeaderInformation();
 
 	std::cout << "Patch Resource Group: " << importParamsPrevious.filename << std::endl;
-	std::cout << "Patch Binaries Base Path: " << Stringify( patchApplyParams.patchBinarySourceSettings.basePaths ) << std::endl;
+	std::cout << "Patch Binaries Base Paths: " << Stringify( patchApplyParams.patchBinarySourceSettings.basePaths ) << std::endl;
 	std::cout << "Patch Binaries Source Type: " << SourceTypeToString( patchApplyParams.patchBinarySourceSettings.sourceType ) << std::endl;
-	std::cout << "Resources To Patch Base Path: " << Stringify( patchApplyParams.resourcesToPatchSourceSettings.basePaths ) << std::endl;
+	std::cout << "Resources To Patch Base Paths: " << Stringify( patchApplyParams.resourcesToPatchSourceSettings.basePaths ) << std::endl;
 	std::cout << "Resources To Patch Source Type: " << SourceTypeToString( patchApplyParams.resourcesToPatchSourceSettings.sourceType ) << std::endl;
 	std::cout << "Next Resources Base Path: " << Stringify( patchApplyParams.newBuildResourcesSourceSettings.basePaths ) << std::endl;
 	std::cout << "Next Resources Source Type: " << SourceTypeToString( patchApplyParams.newBuildResourcesSourceSettings.sourceType ) << std::endl;
--- cli/src/ApplyPatchCliOperation.h
+++ cli/src/ApplyPatchCliOperation.h
@@ -12,9 +12,9 @@ private:
 	void PrintStartBanner(const CarbonResources::ResourceGroupImportFromFileParams& importParamsPrevious, const CarbonResources::PatchApplyParams patchApplyParams) const;
 	bool ApplyPatch(const CarbonResources::ResourceGroupImportFromFileParams& importParamsPrevious, const CarbonResources::PatchApplyParams patchApplyParams) const;
 	std::string m_patchResourceGroupPathArgumentId;
-	std::string m_patchBinariesBasePathArgumentId;
+	std::string m_patchBinariesBasePathsArgumentId;
 	std::string m_patchBinariesSourceTypeArgumentId;
-	std::string m_resourcesToPatchBasePathArgumentId;
+	std::string m_resourcesToPatchBasePathsArgumentId;
 	std::string m_resourcesToPatchSourceTypeArgumentId;
 	std::string m_nextResourcesBasePathArgumentId;
 	std::string m_nextResourcesSourceTypeArgumentId;
