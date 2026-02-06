e5ed360d5f2cdcc0befd38fa367fc3e75d822bd6
CCP ChargeBack <35330827+ccp-chargeback@users.noreply.github.com>
2026-02-06 13:31:41 +0000

    Cleanup, simplify and comment the FilterNamedSection class

--- tools/include/FilterNamedSection.h
+++ tools/include/FilterNamedSection.h
@@ -15,34 +15,50 @@
 namespace ResourceTools
 {
 
+// -------------------------------------------------------------
+// Description:
+// FilterNamedSection is a class that represents all the contents
+// of a named section (e.g: [SomeSectionName]) from a filter .ini file.
+// -------------------------------------------------------------
 class FilterNamedSection
 {
 public:
-	explicit FilterNamedSection( std::string sectionName,
-								 const std::string& filter,
-								 const std::string& respaths,
-								 const std::string& resfile,
+	// Constructs a FilterNamedSection object for the given section.
+	// Using the raw string "filter", "respaths", optional "resfile", and
+	// parent "prefixmap" (from [DEFAULT] section) attributes as inputs.
+	explicit FilterNamedSection( const std::string& sectionName,
+								 const std::string& rawFilter,
+								 const std::string& rawRespaths,
+								 const std::string& rawResfile,
 								 const FilterPrefixMap& parentPrefixMap );
 
+	// Return the name of this section (e.g: [SomeSectionName]) from the .ini file.
 	const std::string& GetSectionName() const;
 
-	// Return combined resolved path map from both respaths and optional resfile
+	// Return the combined resolved path map from both the "respaths" and optional "resfile" attributes.
+	// This is the main function to use to get the final resolved paths and their associated filters for this section.
 	const std::map<std::string, FilterResourceFilter>& GetCombinedResolvedPathMap();
 
+	// Return the resolved path map from the "respaths" attribute. Only used in tests to verify correctness of data.
 	const std::map<std::string, FilterResourceFilter>& GetResolvedRespathsMap() const;
 
+	// Return the resolved path map from the optional "resfile" attribute. Only used in tests to verify correctness of data.
 	const std::map<std::string, FilterResourceFilter>& GetResolvedResfileMap() const;
 
 private:
+	// The name of this section (e.g: [SomeSectionName]) from the .ini file.
 	std::string m_sectionName;
 
+	// The parsed "filter" attribute for this named section.
 	FilterResourceFilter m_filter;
 
+	// The parsed "respaths" attribute.
 	FilterResourcePathFile m_respaths;
 
+	// The optional parsed "resfile" attribute.
 	std::optional<FilterResourcePathFile> m_resfile;
 
-	// Combined map of fully resolved respaths and resfile FilterResourceFilter objects
+	// Combined map of fully resolved "respaths" and "resfile" FilterResourceFilter objects
 	std::map<std::string, FilterResourceFilter> m_resolvedCombinedPathMap;
 };
 
--- tools/src/FilterNamedSection.cpp
+++ tools/src/FilterNamedSection.cpp
@@ -7,51 +7,79 @@
 namespace ResourceTools
 {
 
-FilterNamedSection::FilterNamedSection( std::string sectionName,
-										const std::string& filter,
-										const std::string& respaths,
-										const std::string& resfile,
+// -------------------------------------------------------------
+// Description:
+//   Constructs a FilterNamedSection object for a named section
+//   within a filter .ini file.
+// Arguments:
+//   sectionName - name of the section (e.g: [SomeSectionName])
+//   rawFilter - the "filter" attribute for this section (e.g: "[ .yaml .txt ]")
+//   respaths - the "respaths" attribute of this section (e.g: "prefix:/someSubPath/*")
+//   resfile - the optional "resfile" attribute of this section (e.g: "prefix:/pathToSomeFile.txt")
+//   parentPrefixMap - the FilterPrefixMap from the [DEFAULT] section of the .ini file,
+//     used to resolve prefixes in "respaths" and "resfile" attributes to actual paths.
+// -------------------------------------------------------------
+FilterNamedSection::FilterNamedSection( const std::string& sectionName,
+										const std::string& rawFilter,
+										const std::string& rawRespaths,
+										const std::string& rawResfile,
 										const FilterPrefixMap& parentPrefixMap ) :
-	m_sectionName( std::move( sectionName ) ),
-	m_filter( filter, true ), // isToplevelFilter = true
-	m_respaths( respaths, parentPrefixMap, m_filter ),
-	m_resfile( resfile.empty() ? std::nullopt : std::make_optional<FilterResourcePathFile>( resfile, parentPrefixMap, m_filter ) )
+	m_sectionName( sectionName ),
+	m_filter( rawFilter, true ), // When constructing a [NamedSection], isToplevelFilter should always be true (as it's not an inline filter)
+	m_respaths( rawRespaths, parentPrefixMap, m_filter ),
+	m_resfile( rawResfile.empty() ? std::nullopt : std::make_optional<FilterResourcePathFile>( rawResfile, parentPrefixMap, m_filter ) )
 {
-	m_resolvedCombinedPathMap.clear();
-
-	if( respaths.empty() )
+	if( rawRespaths.empty() )
 	{
 		throw std::invalid_argument( "Respaths attribute is empty for section: " + m_sectionName );
 	}
 }
 
+// -------------------------------------------------------------
+// Description:
+//   Gets the name of this section (e.g: [SomeSectionName]) from the .ini file.
+// Return Value:
+//   String representing the name of the section
+// -------------------------------------------------------------
 const std::string& FilterNamedSection::GetSectionName() const
 {
 	return m_sectionName;
 }
 
+// -------------------------------------------------------------
+// Description:
+//   Gets the combined resolved path map from both the "respaths" and optional "resfile" attributes.
+//   This is the main getter function to use for the full resolved paths
+//   and their associated filters from within this section.
+// Return Value:
+//   Map of resolved paths to their associated FilterResourceFilter objects.
+// Note:
+//   The combined map from both the "respaths" and "resfile" attributes
+//   is populated during the first call to this function (and then cached).
+// -------------------------------------------------------------
 const std::map<std::string, FilterResourceFilter>& FilterNamedSection::GetCombinedResolvedPathMap()
 {
 	// Only populate the Combined map if not already done so.
 	if( m_resolvedCombinedPathMap.empty() )
 	{
-		// Populate the combined map.
+		// Populate the combined map with "respaths" attribute entries first (as they are non-optional)
 		for( const auto& kv : m_respaths.GetResolvedPathMap() )
 		{
 			m_resolvedCombinedPathMap.insert_or_assign( kv.first, kv.second );
 		}
 
-		// Add resfile to the combined map
+		// Add the "resfile" attribute entries to the combined map (in case there are any).
+		// Make sure to combine any filters if the same key already exists from the "respaths" attribute.
 		if( m_resfile )
 		{
-			// Allow "resfile" to contain multiple entries (future proofing)
+			// Allow "resfile" to contain multiple entries (future proofing it, not currently utilized as such in existing .ini filter files)
 			for( const auto& kv : m_resfile->GetResolvedPathMap() )
 			{
-				// Combine filters of both if same key already exists
+				// Combine filters of both if same key already exists (using the raw filter strings).
+				// Else just add the "resfile" attribue entry to the combined map as is.
 				auto it = m_resolvedCombinedPathMap.find( kv.first );
 				if( it != m_resolvedCombinedPathMap.end() )
 				{
-					// Combine the filters (using raw filter strings)
 					std::string combinedRawFilter = it->second.GetRawFilter() + " " + kv.second.GetRawFilter();
 					FilterResourceFilter combinedFilter( combinedRawFilter );
 					m_resolvedCombinedPathMap.insert_or_assign( kv.first, combinedFilter );
@@ -67,11 +95,31 @@ const std::map<std::string, FilterResourceFilter>& FilterNamedSection::GetCombin
 	return m_resolvedCombinedPathMap;
 }
 
+// -------------------------------------------------------------
+// Description:
+//   Gets the resolved path map from the "respaths" attribute only.
+// Return Value:
+//   Map of resolved paths to their associated FilterResourceFilter objects.
+// Note:
+//   Users of this class should use the GetCombinedResolvedPathMap()
+//   function instead of this one to get the "full combined" result.
+//   This function is exposed only to enable tests to verify correctness of data.
+// -------------------------------------------------------------
 const std::map<std::string, FilterResourceFilter>& FilterNamedSection::GetResolvedRespathsMap() const
 {
 	return m_respaths.GetResolvedPathMap();
 }
 
+// -------------------------------------------------------------
+// Description:
+//   Gets the resolved path map from the optional "resfile" attribute only.
+// Return Value:
+//   Map of resolved paths to their associated FilterResourceFilter objects.
+// Note:
+//   Users of this class should use the GetCombinedResolvedPathMap()
+//   function instead of this one to get the "full combined" result.
+//   This function is exposed only to enable tests to verify correctness of data.
+// -------------------------------------------------------------
 const std::map<std::string, FilterResourceFilter>& FilterNamedSection::GetResolvedResfileMap() const
 {
 	if( m_resfile )
